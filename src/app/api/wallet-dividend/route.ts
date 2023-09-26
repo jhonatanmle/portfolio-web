d005f3c27fcdfa2f94f33d182027f363931ef4ad
Jhonatan <jhonatan_ml_1994@hotmail.com>
2023-09-25 22:14:07 -0500

    update files

--- src/app/api/wallet-dividend/route.ts
+++ src/app/api/wallet-dividend/route.ts
@@ -1,5 +1,6 @@
-import { NextRequest, NextResponse } from 'next/server';
+"use server";
 
+import { NextRequest, NextResponse } from 'next/server';
 import { graphqlClient } from '@/features/core/http-client';
 import { queryFindWalletDividendInformation } from '@/features/wallet/services/graphql';
 import { FindWalletInformation } from '@/features/wallet/services/interfaces/find-wallet-information';
@@ -39,26 +40,26 @@ export async function GET(request: NextRequest) {
     );
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
-    console.log(error?.response?.status);
+    // console.log(error?.response?.status);
 
-    if(error?.response?.status === 401){
-      const refreshToken = request.cookies.get('refreshToken');
+    // if(error?.response?.status === 401){
+    //   const refreshToken = request.cookies.get('refreshToken');
 
-      const { accessToken } = await regenerateWalletToken(refreshToken?.value!);
+    //   const { accessToken } = await regenerateWalletToken(refreshToken?.value!);
 
-      const response = await handleHttp({
-        token: accessToken
-      });
+    //   const response = await handleHttp({
+    //     token: accessToken
+    //   });
   
-      return NextResponse.json(
-        {
-          data: response,
-        },
-        {
-          status: 200,
-        }
-      );
-    }
+    //   return NextResponse.json(
+    //     {
+    //       data: response,
+    //     },
+    //     {
+    //       status: 200,
+    //     }
+    //   );
+    // }
 
     return NextResponse.json({
       data: undefined,
